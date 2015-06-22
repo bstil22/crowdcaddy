20a63e4ceeac36c2032006900cbb87ba4b4b1750
bstil22 <bradydynamite@hotmail.com>
2015-06-22 15:56:08 -0700

    added profile page for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
+  resources :users, :only => [:show]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
-
+  get '/search' => 'welcome#query'
   resources :courses do
      resources :holes do
        resources :tips
