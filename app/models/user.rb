2b36b284d2b7f5f6848aad57beca15c6de1456e8
bstil22 <bradydynamite@hotmail.com>
2015-06-28 01:28:05 -0700

    added validations for first name and last name of user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   acts_as_voter
+
+  validates_format_of :first_name, :with => /\A[^0-9`!@#\$%\^&*+_=]+\z/
+  validates_format_of :last_name, :with => /\A[^0-9`!@#\$%\^&*+_=]+\z/
 end
