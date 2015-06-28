053a073607a71acd01becffa89001412a8eb461b
bstil22 <bradydynamite@hotmail.com>
2015-06-28 02:12:32 -0700

    validations for course creation

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,4 +2,8 @@ class Course < ActiveRecord::Base
   has_many :holes
   has_many :tips, through: :holes
   validates :number_holes, presence: true
+  validates :name, presence: true
+  validates :state, presence: true
+  validates :city, presence: true
+  validates :style, presence: true
 end
